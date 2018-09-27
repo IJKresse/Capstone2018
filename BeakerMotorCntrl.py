d1d7082600e7acc0b07969d70283f16cd101ddeb
Isaac Kresse <kresse.i@husky.neu.edu>
2018-09-27 14:53:51 -0400

    Getting Stepper Motor to work

--- BeakerMotorCntrl.py
+++ BeakerMotorCntrl.py
@@ -1,12 +1,34 @@
-from gpiozero import AngularServo
-s = AngularServo(17, min_angle = -180, max_angle = 180) # GPIO pin number
-NUM_BEAKERS = 5;
-angleBtwnBeakers = 360/NUM_BEAKERS
+from gpiozero import OutputDevice
+from time import sleep
+
+STEPS_PER_REV = 200
+STEPS_PER_DEG = STEPS_PER_REV/360
+NUM_BEAKERS = 5
+
+dir = OutputDevice(15)
+pulse = OutputDevice(14)
+
+dir.on()
+
+for i in range(0,200):
+    pulse.on()
+    sleep(0.001)
+    pulse.off()
+    sleep(0.001)
+
+def rotate_deg(deg,motor):
+    numSteps = deg * STEPS_PER_DEG
+    
+    for i in range(0,numSteps):
+        pulse.on()
+        sleep(0.001)
+        pulse.off()
+        sleep(0.001)
 
 # Rotates the disk of beakers by the number of positions 
-def rotate_beakers(numPos):
-    currentAngle = s.value() * 180
-    newAngle = (numPos * angleBtwnBeakers) + currentAngle
-    s.angle(newAngle)
+#def rotate_beakers(numPos):
+#    currentAngle = s.value() * 180
+#    newAngle = (numPos * angleBtwnBeakers) + currentAngle
+#    s.angle(newAngle)
     
 
